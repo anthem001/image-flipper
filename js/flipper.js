2dbd3c08932d3b8293c13e912b874d71186f9e5d
Kelly Martinez <anthem001@gmail.com>
2014-02-08 20:24:19 -0700

    some cleanup in the js file

--- js/flipper.js
+++ js/flipper.js
@@ -1,4 +1,5 @@
 var ImageRotator = function(slider) {
+
     this.images = slider;
 	this.images.style.visibility = "visible";
     this.currentIterator = 0;
@@ -6,53 +7,80 @@ var ImageRotator = function(slider) {
     this.dlArray = this.images.getElementsByTagName("dl");
     this.numSliderImages = this.dlArray.length;
     this.createButtons();
+
 };
 
 ImageRotator.prototype = function() {
+
+    /* hides all images to start */
     var hideAllImages = function() {
+
         var i = 0;
         while (i < this.numSliderImages) {
             this.dlArray[i].style.display = "none";
             i++;
         }
-    }, displayImage = function() {
+
+    }, 
+
+    /* displays image */
+    displayImage = function() {
+
         if (this.currentIterator < this.numSliderImages) {
+            
             //rehide images
             this.hideAllImages();
+            
             //show specific image
             this.dlArray[this.currentIterator].style.display = "block";
             this.highlightButton();
+
             //iterate the index
             this.currentIterator++;
         } else {
+
             this.currentIterator = 0;
+
         }
-    }, createButtons = function() {
+
+    }, 
+
+    /* creates buttons from dl tags */
+    createButtons = function() {
+
         var dl = document.createElement('dl');
         dl.id = "imageButtonList";
         this.images.appendChild(dl);
         var goToNum = this.goToNum;
         var imageButtonList = document.getElementById("imageButtonList");
+
         for (var i = 0; i < this.numSliderImages; i++) {
+
             var iteratorAdjust = i + 1;
             var link = document.createElement('button');
+            var buttons = imageButtonList.childNodes[i];
+
             link.setAttribute('id', 'button' + i);
             link.innerHTML = iteratorAdjust;
             imageButtonList.appendChild(link);
 
-            var buttons = imageButtonList.childNodes[i];
             buttons.onclick = function(e) {
                 buttonMove(e);
             };
+
         }
-    }, goToNum = function(num) {
+    }, 
+
+    goToNum = function(num) {
         var adjustNum = num - 1;
         this.hideAllImages();
         this.currentIterator = adjustNum;
         this.dlArray[adjustNum].style.display = "block";
         this.highlightButton();
         this.currentIterator++;
-    }, highlightButton = function() {
+    }, 
+
+    highlightButton = function() {
         this.clearHighlight();
         var imageButtonList = document.getElementById("imageButtonList").childNodes;
         var currentButton = imageButtonList[this.currentIterator];
@@ -60,7 +88,9 @@ ImageRotator.prototype = function() {
         currentButton.style.color = "#FFF";
         currentButton.style.fontWeight = "900";
 
-    }, clearHighlight = function() {
+    }, 
+
+    clearHighlight = function() {
         var imageButtonList = document.getElementById("imageButtonList").childNodes;
         for ( i = 0; i < imageButtonList.length; i++) {
             var currentButton = imageButtonList[i];
